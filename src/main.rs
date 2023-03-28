69297e939704b724792747b411f463378fcf3d3e
Daniel Hoffmann <daniel.hoffmann@hoffke.de>
2023-03-28 16:27:34 +0200

    parse file to sourcline vec

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    parse_file("test.shk")
+        .iter()
+        .for_each(|source_line| {
+            println!("Line {}: {}", source_line.number, source_line.text);
+        })
 }
+
+
+// kindof just a line of text,
+// except it knows where in the source code it came from
+struct SourceLine<'a> {
+    number: u32,
+    text: String,
+    file_name: &'a str,
+}
+
+// takes an input file and returns a Vec of SourceLine s
+fn parse_file<'a>(file_name: &'a str) -> Vec<SourceLine<'a>> {
+    fs::read_to_string(file_name)
+        .expect("Unable to read file")
+        .split_terminator('\n')
+        .enumerate()
+        .map(|(i, line)| SourceLine {
+            number: i as u32 + 1,
+            text: line.to_string(),
+            file_name: &file_name,
+        })
+        .collect()
+}
+
