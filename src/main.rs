351b5b8cff40297e82d4157b712fb45b0a982183
Daniel Hoffmann <daniel.hoffmann@hoffke.de>
2023-03-28 22:44:04 +0200

    make_indent_nexts

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,59 @@
-use std::fs;
+use std::{fs, iter, ops::Range};
 
 fn main() {
-    parse_file("test.shk")
+    let (nums, lines) = parse_file("test.shk");
+    for (num, line) in iter::zip(nums, &lines) {
+        println!("{} {}", num, line);
+    }
+
+    let indent_nexts = make_indent_nexts(&lines);
+    for i in indent_children(8, indent_nexts) {
+        print!("{} ",i);
+    }
+}
+
+// and indent_next is the index to the next line
+// on the same indentation level,
+// provided all lines in between have a higher indentation level
+fn make_indent_nexts(lines: &Vec<String>) -> Vec<Option<usize>> {
+    let mut indent_nexts = vec![None; lines.len()];
+    let indentation: Vec<usize> = lines
         .iter()
-        .for_each(|source_line| {
-            println!("Line {}: {}", source_line.number, source_line.text);
-        })
+        .map(|line| line.chars().take_while(|char| char.is_whitespace()).count())
+        .collect();
+
+    for i in 0..lines.len() {
+        for j in i + 1..lines.len() {
+            if indentation[j] < indentation[i] {
+                break;
+            }
+
+            if indentation[j] == indentation[i] {
+                indent_nexts[i] = Some(j);
+                break;
+            }
+        }
+    }
+
+    return indent_nexts;
 }
 
-
-// kindof just a line of text,
-// except it knows where in the source code it came from
-struct SourceLine<'a> {
-    number: u32,
-    text: String,
-    file_name: &'a str,
+// given an index i to some line
+// returns an index iter of the children of that line
+fn indent_children(i: usize, indent_nexts: Vec<Option<usize>>) -> Range<usize> {
+    i+1..indent_nexts[i].unwrap()
 }
 
-// takes an input file and returns a Vec of SourceLine s
-fn parse_file<'a>(file_name: &'a str) -> Vec<SourceLine<'a>> {
+// returns all non-empty lines from a file and their numbers
+fn parse_file(file_name: &str) -> (Vec<usize>, Vec<String>) {
     fs::read_to_string(file_name)
         .expect("Unable to read file")
         .split_terminator('\n')
+        .map(String::from)
+        // add line numbers, starting at 1
         .enumerate()
-        .map(|(i, line)| SourceLine {
-            number: i as u32 + 1,
-            text: line.to_string(),
-            file_name: &file_name,
-        })
-        .collect()
+        .map(|(num, line)| (num + 1, line))
+        // ignore comments and empty lines
+        .filter(|(_num, line)| !(line.is_empty() || line.trim().starts_with('#')))
+        .unzip()
 }
-
